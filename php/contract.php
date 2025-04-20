6fbf8990caead83a2335363b696c9819fccd8c3c
ShishantG87 <shishantbohora44@gmail.com>
2025-04-20 15:07:54 -0400

    s

--- php/contract.php
+++ php/contract.php
@@ -11,20 +11,25 @@
 
   <body>
     <?php require_once('components/header.php') ?>
-    
     <main>
-      <p>
-        I,
-        <a href="https://ShishantG87.github.io/intro.html" >Shishant Bohora</a>,
-        agree to abide by the terms in my Spring 2025 WEB250-N850 Database Driven Websites course with my instructor, D.I. von Briesen
-      </p>
-      <p>
-        I understand that all work that I do on my school and personal websites will be publicly available to the world. I will not put information there that is inappropriate for schoolwork, or that I wish to keep private.
-      </p>
-      <p>
-        I also understand that it is my work that counts for attendance, not logins or showing up for class. As such, failure to turn in assignments may show as absences.
-      </p>
-      <p>Signed: Shishant Bohora, 19 January 2025</p>
+    <div id="contract-info">
+        <h2 class="c-head">Contract</h2>
+        <p>
+          I,
+          <a href="intro.php" id="contract-link">Shishant Bohora</a>, agree to
+          abide by the terms in my Spring 2025 WEB250-N850 Database Driven
+          Websites course with my instructor, D.I. von Briesen I understand that
+          all work that I do on my school and personal websites will be publicly
+          available to the world.
+        </p>
+        <p>
+          I will not put information there that is inappropriate for schoolwork,
+          or that I wish to keep private. I also understand that it is my work
+          that counts for attendance, not logins or showing up for class. As
+          such, failure to turn in assignments may show as absences.
+        </p>
+        <p>Signed: <em>Shishant Bohora, 19 January 2025</em></p>
+      </div>
     </main>
     <?php require_once('components/footer.php') ?>
     
